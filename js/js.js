5c0ca9c86ad98c2d553e6003076804e9e83c5797
htmleros <htmlerosavjk@gmail.com>
2025-04-22 15:20:38 -0300

    JS

---
+++ js/js.js
@@ -0,0 +1,9 @@
+function avisoPagContruccion() {
+    alert("Página en contruccón")
+}
+
+function avisoTemp(hora) {
+    alert(`La pagina no esta funcionando correctamente, vuelva a las ${hora} hrs.`)
+}
+
+avisoTemp("15:29");
