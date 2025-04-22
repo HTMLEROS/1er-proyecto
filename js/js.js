bf771dd6740d2e3dc5bd9dbe6903f251231a7d12
htmleros <htmlerosavjk@gmail.com>
2025-04-22 15:47:01 -0300

    Agregado de varios directorios JS y un aviso de mantenimiento en prueba, tambien, se crei mantenimiento.html

--- js/js.js
+++
@@ -1,9 +0,0 @@
-function avisoPagContruccion() {
-    alert("Página en contruccón")
-}
-
-function avisoTemp(hora) {
-    alert(`La pagina no esta funcionando correctamente, vuelva a las ${hora} hrs.`)
-}
-
-avisoTemp("15:29");
